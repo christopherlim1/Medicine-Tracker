b39f8a91ce099d66cdf9682f53667149c1e9ad6c
cblim <cblim@ucsc.edu>
2021-10-10 18:33:42 -0700

    set up react and semantic-ui

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Home from './Home';
+import 'semantic-ui-css/semantic.min.css';
+
+function App() {
+    return (
+        <div>
+            <Home/>
+        </div>
+    );
+}
+
+export default App;
