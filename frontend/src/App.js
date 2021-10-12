4eb8d644277152ff5f75b02bac863694bdfa81e2
glaguina <glaguina@ucsc.edu>
2021-10-12 14:45:58 -0700

    editing src files for frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import Home from './Home';
-import LoginForm from './LoginForm';
+import Login from './Login';
 import 'semantic-ui-css/semantic.min.css';
 
 function App() {
-    return (
-        <div>
+  return (
+    <div>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/'>
+            <Login/>
+          </Route>
+          <Route path="/homepage" exact>
             <Home/>
-        </div>
-    );
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
