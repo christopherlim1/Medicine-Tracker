70123a23385de42e23ff4394c1fd14410422563b
glaguina <glaguina@ucsc.edu>
2021-10-12 15:24:24 -0700

    fixed App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Home from './Home';
 import Login from './Login';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import 'semantic-ui-css/semantic.min.css';
 
 function App() {
@@ -11,7 +12,7 @@ function App() {
           <Route exact path='/'>
             <Login/>
           </Route>
-          <Route path="/homepage" exact>
+          <Route path="/home" exact>
             <Home/>
           </Route>
         </Switch>
