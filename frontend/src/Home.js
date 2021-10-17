5188675206c0c7c0b160f866de2be0fe2453c575
Artyom Martirosyan <art@Artyoms-MacBook-Pro.local>
2021-10-17 14:46:06 -0700

    started top bar having some issues with positioning and need to change style

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,11 +1,17 @@
 import React from 'react';
-
+import { Button, Form, Grid, Header, Image, Message, Segment } from 'semantic-ui-react'
 function Home() {
     return (
-        <div>
-            Start of the Medicine Tracker
+        <div style = {{backgroundColor: 'red', height: '100px'}}>
+        
+            <Header as='h2' color='blue' textAlign='center' margintop='0'>
+                <Image class="ui mini image" src='/healthSlug.png'/>
+                <h1 style = {{fontSize: '70px', whiteSpace:'nowrap',  display:'inline'}}>Medicine tracker</h1>
+                <Image style = {{position: 'fixed', right: '0'}} class="ui mini image" src='/healthSlug.png'/>
+            </Header>
         </div>
     );
 }
 
 export default Home;
+
