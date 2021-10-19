fd04df51fb024a3ea49fc58cc38daa293a134a9f
cblim <cblim@ucsc.edu>
2021-10-19 16:10:33 -0700

    MATERIAUL UI BABY

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Home from './Home';
 import Login from './Login';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
-import 'semantic-ui-css/semantic.min.css';
 
 function App() {
   return (
--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -1,39 +1,8 @@
 import React from 'react'
-import { Button, Form, Grid, Header, Image, Message, Segment } from 'semantic-ui-react'
 
 function Login() {
   return (
-    <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
-      <Grid.Column style={{ maxWidth: 450 }}>
-        <Header as='h2' color='blue' textAlign='center'>
-          <Image class="ui mini image" src='/healthSlug.png' /> Log-in to your account
-        </Header>
-        <Form size='large'>
-          <Segment stacked>
-            <Form.Input 
-              fluid 
-              icon='user' 
-              iconPosition='left' 
-              placeholder='E-mail address' 
-            />
-            <Form.Input
-              fluid
-              icon='lock'
-              iconPosition='left'
-              placeholder='Password'
-              type='password'
-            />
-
-            <Button color='blue' fluid size='large'>
-              Login
-            </Button>
-          </Segment>
-        </Form>
-        <Message>
-          New to us? <a href='#'>Sign Up</a>
-        </Message>
-      </Grid.Column>
-    </Grid>
+    <div>Login</div>
   );
 }
 
