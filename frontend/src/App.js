be265c5cd7ea3d0e61d8528ec94ce18113e4f8ce
alexjkinder <92126656+alexjkinder@users.noreply.github.com>
2021-10-20 13:54:25 -0700

    Added Calendar file and created calendar
    Created a Calendar file with a basic calendar in the frontend fold. I connected the calendar to App.js so it can be viewable with ./calendar extension.
    
    Currently the calendar does NOT support Events.
    
    TODO: Add functionality to add, delete, and view events.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Home from './Home';
 import Login from './Login';
+import Calendar from './Calendar';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
 
 function App() {
@@ -13,6 +14,9 @@ function App() {
           </Route>
           <Route path="/home" exact>
             <Home/>
+          </Route>
+          <Route path="/calendar" exact>
+            <Calendar/>
           </Route>
         </Switch>
       </BrowserRouter>
