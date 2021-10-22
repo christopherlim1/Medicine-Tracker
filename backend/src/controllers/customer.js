dc0c34d37f3a225ae4a7e4db181912675b920197
Juan Lee <69443239+JUANie10@users.noreply.github.com>
2021-10-21 18:43:26 -0700

    GET and POST reqiuests are working
    For Medicine and Customer

--- backend/src/controllers/customer.js
+++ backend/src/controllers/customer.js
@@ -1,22 +1,23 @@
 const CustomerInfo = require('../models/customerInfo.js');
 
-// POST /customer
+// POST /customer/:googleID
 exports.createCustomer = async (req, res) => {
     try {
         const newCustomer = new CustomerInfo(req.body);
+        newCustomer['googleID'] = req.params.googleID;
         await newCustomer.save();
-        res.status(201).json(req.body);
+        res.status(201).json(newCustomer);
     } catch(error) {
         res.status(409).json({message: error.message});
     }
 };
 
-// GET /:customer
+// GET /customer/:googleID
 exports.getCustomer = async (req, res) => {
     try {
-        const customerID = req.params.customer;
-        const customer = await CustomerInfo.find({googleID: customerID})
-        res.status(201).json(customer);
+        const googleID = req.params.googleID;
+        const customer = await CustomerInfo.find({googleID: googleID})
+        res.status(200).json(customer[0]);
     } catch(error) {
         res.status(409).json({message: error.message});
     }
