a308f823cb9e07e3b1925eba79fb9025f406f98c
Artyom Martirosyan <art@eduroam-169-233-247-105.ucsc.edu>
2021-10-26 17:03:48 -0700

    implemented basic medlist format

--- frontend/src/components/MedList.js
+++ frontend/src/components/MedList.js
@@ -12,22 +12,68 @@ import ExpandLess from '@mui/icons-material/ExpandLess';
 import ExpandMore from '@mui/icons-material/ExpandMore';
 import StarBorder from '@mui/icons-material/StarBorder';
 
+const meds = [
+{'name': 'Advil', 'Dosage': 'Dosage: 3 days a week', 'Today': 'Need to take today at 2pm'},
+{'name': 'Tylenol', 'Dosage': 'Dosage: 1 day a week', 'Today': 'Need to take mondays at 1am'},
+{'name': 'Codine', 'Dosage': 'Dosage: 1 day a week', 'Today': 'Need to take saturday at 2pm'},
+{'name': 'Nyquil', 'Dosage': 'Dosage: 1 day a week', 'Today': 'Need to take saturday at 2pm'},
+{'name': 'Dayquil', 'Dosage': 'Dosage: 1 day a week', 'Today': 'Need to take saturday at 2pm'},
+];
+/*
+const [open, setOpen] = React.useState(true);
+
+const handleClick = () => {
+  setOpen(!open);
+};
+*/
+function MedDetails(index) {
+  const arr = [];
+  const keys = ['Dosage', 'Today'];
+  for (let i = 0; i < keys.length; i++) {
+    const name = meds[index][keys[i]];
+    const jsx =
+      <ListItemButton sx={{ pl: 4 }}>
+            <ListItemIcon>
+              <StarBorder />
+            </ListItemIcon>
+            <ListItemText primary={name} />
+      </ListItemButton>
+    arr.push(jsx);
+  }
+  return arr;
+};
+
+
+
+// https://stackoverflow.com/questions/55622768/uncaught-invariant-violation-rendered-more-hooks-than-during-the-previous-rende
 function MedList() {
-  const [open, setOpen] = React.useState(true);
+  //const meds = ["Advil", "Tylenol", "Codine", "Nyquil", "DayQuil"];
+  const arr = [];
+  for (let i = 0; i < meds.length; i++) {
+    const [open, setOpen] = React.useState(false); // Maybe set to false
+    const name = meds[i]['name'];
+    const jsx =
+    <List
+      sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}
+      component="nav"
+      aria-labelledby="nested-list-subheader"
+    >
+      <ListItemButton onClick={() => setOpen(!open) }>
+        <ListItemIcon>
+          <InboxIcon />
+        </ListItemIcon>
+        <ListItemText primary={name} />
+        {open ? <ExpandLess /> : <ExpandMore />}
+      </ListItemButton>
+      <Collapse in={open} timeout="auto" unmountOnExit>
+        <List component="div" disablePadding>
+          {MedDetails(i)}
+        </List>
+      </Collapse>
+    </List>
+    arr.push(jsx);
+  }
 
-  const handleClick = () => {
-    setOpen(!open);
-  };
-
-  // const [medFeed, setMedFeed] = React.useState([]);
-
-  // fetch medicine depending on customer
-  // // most likely google ID
-  // const fetchMedFeed = (setMedFeed, gID) => {
-
-  // };
-
-  const meds = ["Advil", "Tylenol", "Codine", "Nyquil", "DayQuil"];
   return (
     <List
       sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}
@@ -39,26 +85,7 @@ function MedList() {
         </ListSubheader>
       }
     >
-      {meds.map(med => (
-        <ListItemButton onClick={handleClick}>
-          <ListItemIcon>
-            <InboxIcon />
-          </ListItemIcon>
-          <ListItemText primary={med} />
-        {open ? <ExpandLess /> : <ExpandMore />}
-
-        <Collapse in={open} timeout="auto" unmountOnExit>
-        <List component="div" disablePadding>
-          <ListItemButton sx={{ pl: 4 }}>
-            <ListItemIcon>
-              <StarBorder />
-            </ListItemIcon>
-            <ListItemText primary="Starred" />
-          </ListItemButton>
-        </List>
-      </Collapse>
-      </ListItemButton>
-      ))}
+      {arr}
     </List>
   );
 }
@@ -67,3 +94,62 @@ export default MedList;
 
 
 // https://mui.com/components/lists/
+/*
+function MedDetails(index) {
+  const arr = [];
+  const keys = ['Dosage', 'Today'];
+  for (let i = 0; i < keys.length; i++) {
+    const name = meds[index][keys[i]];
+    const jsx =
+      <ListItemButton
+        onClick={() => {
+        }}
+        key={name}
+        button>
+        <ListItemIcon>
+          <DraftsIcon />
+        </ListItemIcon>
+        <ListItemText primary={name} />
+      </ListItemButton>;
+    arr.push(jsx);
+  }
+  return arr;
+};
+
+function MedList() {
+  const arr = [];
+    for (let i = 0; i < meds.length; i++) {
+      const name = meds[i]['name'];
+      const jsx =
+        <ListItemButton
+          onClick={() => {
+          }}
+          key={name}
+          button>
+          <ListItemIcon>
+            <InboxIcon />
+          </ListItemIcon>
+          <ListItemText primary={meds[i]['name']} />
+        </ListItemButton>;
+        <Collapse in={true} timeout="auto" unmountOnExit>
+          <List component="div" disablePadding>
+            {MedDetails(i)}
+          </List>
+      </Collapse>
+      arr.push(jsx);
+    }
+    return (
+      <List
+        sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}
+        component="nav"
+        aria-labelledby="nested-list-subheader"
+        subheader={
+          <ListSubheader component="div" id="nested-list-subheader">
+            Nested List Items
+          </ListSubheader>
+        }
+      >
+        {arr}
+      </List>
+    );
+}*/
