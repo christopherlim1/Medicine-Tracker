7121f641b424b9f8ab9359e5af22403c362635c9
alexjkinder <92126656+alexjkinder@users.noreply.github.com>
2021-11-01 12:00:16 -0700

    Create eventinfo.js
    Created eventinfo.js in backend models folder.

---
+++ backend/src/models/eventinfo.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const eventSchema = mongoose.Schema({
+    googleID: String,
+});
+
+const EventInfo = mongoose.model('EventInfo', eventSchema);
+
+module.exports = EventInfo;
