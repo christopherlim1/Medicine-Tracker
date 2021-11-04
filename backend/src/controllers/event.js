ca77237599c947756b35caa70d27222ff4ad80ff
alexjkinder <92126656+alexjkinder@users.noreply.github.com>
2021-11-03 17:45:47 -0700

    Update event.js
    WIP. Updated event.js to include create, delete, update, and  get methods. None of these methods have been tested.  updateEvent needs additional fields to be completed.

--- backend/src/controllers/event.js
+++ backend/src/controllers/event.js
@@ -2,7 +2,7 @@ const CustomerInfo = require('../models/customerInfo.js');
 const EventInfo = require('../models/eventInfo.js');
 
 // GET /event/:googleID
-exports.getMedicine = async (req, res) => {
+exports.getEvent = async (req, res) => {
     const googleID = req.params.googleID;
     const myEvent = await EventInfo.find({googleID: googleID});
     if(myEvent.length > 0) {
@@ -10,4 +10,31 @@ exports.getMedicine = async (req, res) => {
     } else {
         res.status(404).send();
     }
+};
+
+// POST /event/:googleID
+exports.createEvent = async (req, res) => {
+    try {
+        const googleID = req.params.googleID;
+        const newEvent = new EventInfo(req.body);
+        newEvent['googleID'] = googleID;
+        await newEvent.save();
+        res.status(201).json(newEvent);
+    } catch(error) {
+        res.status(409).json({message: error.message});
+    }
+};
+
+// DELETE /event/:eventID
+exports.deleteEvent = async (req, res) => {
+    await EventInfo.findByIdAndRemove(req.params.eventID);
+    res.status(200).send('Event is Deleted');
+};
+
+// UPDATE /event/:id
+exports.updateEvent = async (req, res) => {
+    const Event = await EventInfo.findById(req.params.eventID);
+    Event.title = req.body.title;
+    Event.save();
+    res.status(201).json(Event);
 };
