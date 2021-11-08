7a7288968f4c3fe8c10388521997c7dd8a54324f
alexjkinder <92126656+alexjkinder@users.noreply.github.com>
2021-11-07 17:25:20 -0800

    Update eventinfo.js
    Added eventinfo.js

--- backend/src/models/eventinfo.js
+++ backend/src/models/eventinfo.js
@@ -1,8 +1,20 @@
 const mongoose = require('mongoose');
 
 const eventSchema = mongoose.Schema({
-    googleID: String,
+    id: String,
     title: String,
+    groupid: String,
+    start: String,
+    end: String,
+    allDay: Boolean,
+    editable: Boolean,
+    display: String,
+    daysOfWeek: [String], // Not sure if this is the right declaration
+    startTime: String,
+    endTime: String,
+    // startRecur: ,
+    // endRecur: ,
+
 });
 
 const EventInfo = mongoose.model('EventInfo', eventSchema);
