3d603bef90975f1a9491a86a7ae3cdafcc401363
glaguina <glaguina@ucsc.edu>
2021-11-18 22:41:17 -0800

    fixed medList

--- frontend/src/components/MedList.js
+++ frontend/src/components/MedList.js
@@ -15,28 +15,28 @@ import Box from '@mui/material/Box';
 import {WorkspaceContext} from '../App.js';
 
 // let meds = [];
-// function MedDetails(medication) {
-//   const arr = [];
-//   const keys = ['description', 'frequency', 'doses', 'totalAmount', 'time'];
-//   keys.shift();
-//   for (let i = 0; i < keys.length; i++) {
-//     const jsx =
-//       <ListItemButton key={i+2} sx={{ pl: 4 }}>
-//             <ListItemIcon key={i+3}>
-//               <StarBorder />
-//             </ListItemIcon>
-//             <ListItemText primary={keys[i]} secondary={medication[keys[i]]} key={i+4} />
-//       </ListItemButton>
-//     arr.push(jsx);
-//   }
-//   return arr;
-// };
+function MedDetails(medication) {
+  const arr = [];
+  const keys = ['description', 'frequency', 'doses', 'totalAmount', 'time'];
+  keys.shift();
+  for (let i = 0; i < keys.length; i++) {
+    const jsx =
+      <ListItemButton key={i+2} sx={{ pl: 4 }}>
+            <ListItemIcon key={i+3}>
+              <StarBorder />
+            </ListItemIcon>
+            <ListItemText primary={keys[i]} secondary={medication[keys[i]]} key={i+4} />
+      </ListItemButton>
+    arr.push(jsx);
+  }
+  return arr;
+};
 
 const getMedicine = (gID, setMedicineList) => {
   axios.get(`http://localhost:4000/v0/medicine/${gID}`)
     .then((response)=>{
       console.log(response.data);
-      setMedicineList(JSON.stringify(response.data));
+      setMedicineList(response.data);
     })
     .catch(()=>{
       console.log('Cannot get medicine list');
@@ -48,68 +48,73 @@ function MedList() {
   const {customerIDS, medicineListS} = React.useContext(WorkspaceContext);
   const [googleID,] = customerIDS;
   const [medicineList, setMedicineList] = medicineListS;
-  // const [open, setOpen] = React.useState(false); // Maybe set to false
 
   React.useEffect(() => {
     console.log('useEffect getMedicine');
     getMedicine(googleID, setMedicineList);
   }, [googleID, setMedicineList]);
 
-  // const handleOpen = () => {
-  //   setOpen(!open);
-  // };
-
-
   console.log('medList(before for_loop)',medicineList);
 
   // we JSON.stingify()'d contents of medicineList.
-  return (
-    <Box sx={{position: 'relative', top: '60px', width: '100vw'}}>
-      {medicineList}
-    </Box>
-  );
-
-  // const arr = [];
-  // for (let i = 0; i < medicineList.size(); i++) {
-  //   console.log('stuck');
-  //   const name = medicineList[i]['name'];
-  //   const jsx =
-  //   <List
-  //     sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}
-  //     component="nav"
-  //     aria-labelledby="nested-list-subheader"
-  //     key={i}
-  //   >
-  //     <ListItemButton key={i+5} onClick={handleOpen}>
-  //       <ListItemIcon>
-  //         <MedicationIcon/>
-  //       </ListItemIcon>
-  //       <ListItemText primary={name} />
-  //       {open ? <ExpandLess /> : <ExpandMore />}
-  //     </ListItemButton>
-  //     <Collapse in={open} timeout="auto" unmountOnExit>
-  //       <List key={i+1} component="div" disablePadding>
-  //         {MedDetails(medicineList[i])}
-  //       </List>
-  //     </Collapse>
-  //   </List>
-  //   arr.push(jsx);
-  // }
-
   // return (
-  //   <List
-  //     sx={{ width: '100%', maxLength: 360, maxWidth: 360, bgcolor: 'background.paper', overflow: 'auto'}}
-  //     component="nav"
-  //     aria-labelledby="nested-list-subheader"
-  //     subheader={
-  //       <ListSubheader component="div" id="nested-list-subheader">
-  //         Nested List Items
-  //       </ListSubheader>
-  //     }
-  //   >
-  //     {arr}
-  //   </List>
+  //   <Box sx={{position: 'relative', top: '60px', width: '100vw'}}>
+  //     {medicineList}
+  //   </Box>
   // );
+
+  const [open, setOpen] = React.useState(false); // Maybe set to false
+
+  const handleOpen = () => {
+    setOpen(!open);
+  };
+
+  const MedicineElems = () => {
+    const arr = [];
+    const meds = medicineList;
+    for (let i = 0; i < meds.length; i++) {
+      const name = meds[i]['name'];
+      const details = MedDetails(meds[i]);
+      const jsx =
+      <List
+        sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}
+        component="nav"
+        aria-labelledby="nested-list-subheader"
+        key={i}
+      >
+        <ListItemButton key={i+5} onClick={handleOpen}>
+          <ListItemIcon>
+            <MedicationIcon/>
+          </ListItemIcon>
+          <ListItemText primary={name} />
+          {open ? <ExpandLess /> : <ExpandMore />}
+        </ListItemButton>
+        <Collapse in={open} timeout="auto" unmountOnExit>
+          <List key={i+1} component="div" disablePadding>
+            {details}
+          </List>
+        </Collapse>
+      </List>
+      arr.push(jsx);
+    }
+
+    return arr;
+  }
+
+  return (
+    <List
+      sx={{ width: '100%', maxLength: 360, maxWidth: 360, bgcolor: 'background.paper', overflow: 'auto'}}
+      component="nav"
+      aria-labelledby="nested-list-subheader"
+      subheader={
+        <ListSubheader component="div" id="nested-list-subheader">
+          Nested List Items
+        </ListSubheader>
+      }
+    >
+      <MedicineElems/>
+    </List>
+  );
 }
 
 export default MedList;
