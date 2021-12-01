4447cee803cc5b3ce4f2405e1463356a463257db
glaguina <glaguina@ucsc.edu>
2021-11-30 18:14:54 -0800

    working medform

--- backend/src/routes/medicineRoutes.js
+++ backend/src/routes/medicineRoutes.js
@@ -3,10 +3,11 @@ const router = express.Router();
 
 const medicine = require('../controllers/medicine.js');
 
-router.get('/:googleID', medicine.getMedicine);
+router.get('/:googleID', medicine.getAllMedicine);
 router.post('/:googleID', medicine.createMedicine);
 router.delete('/delete/:medicineID', medicine.deleteMedicine);
 router.put('/update/:medicineID', medicine.updateMedicine);
 router.get('/events/:googleID', medicine.getEvents);
+router.get('/single/:medicineID', medicine.getMedicine);
 
 module.exports = router;
