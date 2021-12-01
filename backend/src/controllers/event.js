9354f3c6bd888630d4d535f25b27ebb5093ecc8f
cblim <cblim@ucsc.edu>
2021-12-01 15:55:34 -0800

    Done?

--- backend/src/controllers/event.js
+++ backend/src/controllers/event.js
@@ -1,37 +1,4 @@
-// const EventInfo = require('../models/eventInfo.js');
 const MedicineInfo = require('../models/medicineInfo.js');
-
-// // GET /event/:googleID
-// exports.getEvent = async (req, res) => {
-//     const googleID = req.params.googleID;
-//     const myEvent = await EventInfo.find({googleID: googleID});
-//     if(myEvent.length > 0) {
-//         res.status(200).json(myEvent);
-//     } else {
-//         res.status(404).send();
-//     }
-// };
-
-// // POST /event/:googleID
-// exports.createEvent = async (req, res) => {
-//     try {
-//         const googleID = req.params.googleID;
-//         const newEvent = new EventInfo(req.body);
-//         newEvent['googleID'] = googleID;
-//         await newEvent.save();
-//         res.status(201).json(newEvent);
-//     } catch(error) {
-//         res.status(409).json({message: error.message});
-//     }
-// };
-
-// // DELETE /event/:eventID
-// exports.deleteEvent = async (req, res) => {
-//     await EventInfo.findByIdAndRemove(req.params.eventID);
-//     res.status(200).send('Event is Deleted');
-// };
-
-
 
 // UPDATE /v0/event/update/:googleID/:eventID
 exports.updateEvent = async (req, res) => {
@@ -52,10 +19,8 @@ exports.updateEvent = async (req, res) => {
             }
         }
     }
-    // console.log(Medicine[index]);
     Medicine[index].events = [];
     Medicine[index].events = eventsArray;
-    // console.log(Medicine[index]);
     Medicine[index].save();
     console.log(Medicine[index]);
     res.status(201).json(Medicine);
